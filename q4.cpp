e11a07eaa71f839bcb7f225a790883ce663793de
fkstndnjs <fkstndnjs@naver.com>
2021-03-26 16:18:10 +0900

    q4.cpp

---
+++ q4.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	int i;
+
+	srand((int)time(NULL));
+	
+	for(i=0; i<100; i++)
+		printf("정수 출력: %d \n", rand()%3); 
+
+	return 0;
+}
