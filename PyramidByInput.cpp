9dc17df1f79d3f87568717cf73e5172805daf8cf
fkstndnjs <fkstndnjs@naver.com>
2021-03-29 13:18:30 +0900

    PyramidByInput.cpp

---
+++ PyramidByInput.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+	int a;
+	
+	scanf("%d", &num);
+	
+	a=num;
+	
+	for(int i=1; i<num*2; i++)
+	{
+		if(i%2==0)
+			continue;
+		
+		for(int b=a; b>0; b--)
+			printf(" ");
+		
+		for(int j=0; j<i; j++)
+		{
+				printf("*");
+		}
+		
+		a--;
+		
+		printf("\n");
+	}
+	
+	return 0;
+	
+}
