23412deff35bfe4ef3085d6d06f8c0f68fec4bb0
fkstndnjs <fkstndnjs@naver.com>
2021-03-30 16:50:27 +0900

    StructPointer.cpp

---
+++ StructPointer.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct point
+{
+	int xpos;
+	int ypos;
+};
+
+int main(void)
+{
+	struct point pos1={1, 2};
+	struct point* ptr=&pos1;
+	
+	(*ptr).xpos+=4;
+	(*ptr).ypos+=5;
+	printf("%d, %d", ptr->xpos, ptr->ypos);
+	
+	return 0;
+}
