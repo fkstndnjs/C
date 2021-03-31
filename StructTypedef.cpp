2ea1f7301472feae2fe45a7d0b9ffb73d28a70e6
fkstndnjs <fkstndnjs@naver.com>
2021-03-31 14:40:11 +0900

    2021-03-31

--- StructTypedef.cpp
+++ StructTypedef.cpp
@@ -19,8 +19,11 @@ typedef struct Person
 int main(void)
 {
 	Point pos={10, 20};
+	
 	Person man={"À¯¼®Çö", "010-9419-7640", 24};
+	
 	printf("%d %d \n", pos.xpos, pos.ypos);
+	
 	printf("%s %s %d \n", man.name, man.phoneNum, man.age); 
 	
 	return 0;
