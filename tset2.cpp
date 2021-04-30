aa2528be3f4fd1b7a589b52b8170adb549de9dc0
fkstndnjs <fkstndnjs@naver.com>
2021-04-30 11:17:28 +0900

    2021-04-30

--- tset2.cpp
+++ tset2.cpp
@@ -4,14 +4,19 @@ struct point
 {
 	int xpos;
 	int ypos;
+	struct point* ptr;
 };
 
 int main(void)
 {
-	struct point pos={10,20};
-	struct point* ptr=&pos;
+	struct point pos1={1,1};
+	struct point pos2={2,2};
 	
-	printf("%d, %d", (*ptr).xpos, ptr->xpos);
+	pos1.ptr=&pos2;
+	pos2.ptr=&pos1;
+	
+	printf("%d, %d, %d, %d\n", pos1.xpos, pos1.ypos, pos1.ptr->xpos, pos1.ptr->ypos);
+	printf("%d, %d, %d, %d", pos2.xpos, pos2.ypos, pos2.ptr->xpos, pos2.ptr->ypos);
 	
 	return 0;
 }
