da5bdb851a2c8f7b350091afe3a90143d393847b
fkstndnjs <fkstndnjs@naver.com>
2021-05-03 13:56:59 +0900

    2021-05-03

--- tset2.cpp
+++ tset2.cpp
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
-struct point
+typedef enum syllable
 {
-	int xpos;
-	int ypos;
-	struct point* ptr;
-};
+	Do=1, Re=2, Mi=3
+} Syllable;
 
 int main(void)
 {
-	struct point pos1={1,1};
-	struct point pos2={2,2};
+	Syllable tone;
+
+	tone=Do;
 	
-	pos1.ptr=&pos2;
-	pos2.ptr=&pos1;
+	printf("%d", tone);
 	
-	printf("%d, %d, %d, %d\n", pos1.xpos, pos1.ypos, pos1.ptr->xpos, pos1.ptr->ypos);
-	printf("%d, %d, %d, %d", pos2.xpos, pos2.ypos, pos2.ptr->xpos, pos2.ptr->ypos);
+	printf("%d", Do);
 	
 	return 0;
 }
