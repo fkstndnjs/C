1a59a26badc49a7e9561d34f5925f499c086c95a
fkstndnjs <fkstndnjs@naver.com>
2021-05-07 11:10:19 +0900

    2021-05-07

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
-#include "stdiv.h"
-#include "intdiv.h"
+
+typedef struct div
+{
+	int quotient;
+	int remainder;
+} Div;
+
+extern Div IntDiv(int num1, int num2);
 
 int main(void)
 {
-	Div val=IntDiv(5, 2);
+	Div val = IntDiv(5,2);
 	printf("%d, %d", val.quotient, val.remainder);
+	
 	return 0;
 }
